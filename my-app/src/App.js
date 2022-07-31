4b61efcfe871c48bdfcea4110b98a105c51a0a96
ryker-dev <pizzuti.dev@gmail.com>
2022-07-31 03:34:11 +0300

    CodeGrade annoying

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyContainer from './components/MyContainer';
-//import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 function App() {
   return (
